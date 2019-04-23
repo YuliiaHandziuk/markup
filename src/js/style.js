783217fe9538ac1d3fc63050ae8f3760d3688518
YuliiaHandziuk <yuliia.handziuk@gmail.com>
2019-04-23 14:53:41 +0300

    add responsive

--- src/js/style.js
+++ src/js/style.js
@@ -1,3 +1,5 @@
+//changed header by scrolling page
+
 $(function(){
 	var navbar = $('#header');
 	
@@ -9,3 +11,43 @@ $(function(){
 		}
 	});
 });
+
+// burger menu for header (responsive for phone)
+
+$(".nav-opener").click(function() {
+	$(".nav-slide").slideToggle(400);
+  });
+  
+  $(window).resize(function() {
+	if ($(window).width() > 768) {
+	  $(".nav-slide").show();
+	  $(".nav-opener").hide();
+	} else {
+	  $(".nav-slide").hide();
+	  $(".nav-opener").show();
+	}
+  });
+
+
+
+
+//tabs 
+
+jQuery(document).ready(function($) {
+	$('.tab-content').hide();
+	$('.tab-content:first').show();
+	$('.tabset li:first').addClass('active');
+
+	$('.tabset li').click(function(event){
+		$('.tabset li').removeClass('active');
+		$(this).addClass('active');
+		$('.tab-content').hide();
+
+		var selectTab = $(this).find('a').attr('href');
+		$(selectTab).fadeIn();
+	});
+});
+
+ 
+	
+	
